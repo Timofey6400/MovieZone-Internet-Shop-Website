ff932dc48bb4cfcda2dee93250095032b3d33be1
Timofey <clashingtimarcoc@gmail.com>
2026-01-15 16:31:49 +0200

    best variant of site

---
+++ MovieZone/films/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Category, Platform, Film
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "icon", "order", "is_active", "films_count")
+    list_editable = ("order", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name", "description")
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("name", "slug", "icon", "description")
+        }),
+        ("Настройки", {
+            "fields": ("order", "is_active")
+        }),
+    )
+
+    def films_count(self, obj):
+        """Отображает количество фильмов в категории"""
+        return obj.get_films_count()
+    films_count.short_description = "Фильмов"
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "price", "featured")
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("category", "platforms", "featured")
+    search_fields = ("title", "short_description", "description")
